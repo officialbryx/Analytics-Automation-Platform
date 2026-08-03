a561d61b720054e887186477e11ab0d36344326c
bryx <bryxph@gmail.com>
2026-08-03 12:03:50 +0800

    fix: manage.py and entrypoint.sh structure

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --noinput
-gunicorn cdext.wsgi:application --bind 0.0.0.0:8000 $@
+gunicorn aap.wsgi:application --bind 0.0.0.0:8000 $@
